ebdaad1291f55453fd45ae01d8fabfcc8fdc4b4a
j_mcc1993 <j.mcc3093@gmail.com>
2023-03-23 09:50:42 -0700

    Trying more things

--- src/plugin/metro.js
+++ src/plugin/metro.js
@@ -1,5 +1,3 @@
-import Metro from "metro";
-
 const babelTransformerPath = require.resolve("./transformer");
 const codeExts = ["js", "ts", "jsx", "tsx", "mjs", "cjs"];
 const htmlExts = ["htm", "html", "css"];
@@ -14,13 +12,45 @@ const sourceExts = [
   "wasm",
 ];
 
+
 export const bundle = async (filename) => {
-  const config = await Metro.loadConfig();
+  const { getDefaultConfig } = require('@expo/metro-config')
+  const path = require('path')
+
+  const workspaceRoot = path.resolve(__dirname, '../..')
+  const projectRoot = __dirname
+  const config = getDefaultConfig(projectRoot)
+
+  const watchFolders = [workspaceRoot]
+
+  config.resolver.nodeModulesPaths = [path.resolve(projectRoot, 'node_modules')]
+
+  // config.resolver.sourceExts.push('cjs', 'svg')
+  // // config.resolver.sourceExts.push('cjs')
+  // config.resolver.assetExts = config.resolver.assetExts.filter(
+  //   (ext) => ext !== 'svg'
+  // )
+
+  // config.transformer.babelTransformerPath = require.resolve(
+  //   'react-native-react-bridge/lib/plugin'
+  // )
+  config.transformer.babelTransformerPath = babelTransformerPath;
+  config.transformer.getTransformOptions = async () => ({
+    ...config.transformer,
+    transform: {
+      experimentalImportSupport: false,
+      inlineRequires: false,
+    },
+  })
+  config.transformer.plugins = [
+    '@babel/plugin-proposal-numeric-separator',
+    '@babel/plugin-transform-literals',
+  ]
+  config.watchFolders = watchFolders
   config.resolver.sourceExts = sourceExts;
   config.resolver.assetExts = config.resolver.assetExts.filter(
-    (ext) => ext !== 'svg'
+    (ext) => !sourceExts.includes(ext)
   );
-  config.transformer.babelTransformerPath = babelTransformerPath;
 
   const { code, map } = await Metro.runBuild(config, {
     entry: filename,
